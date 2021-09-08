36c9e012803cd2d9f8822e12b1698f2028b2f22e
Trasqual <59961702+Trasqual@users.noreply.github.com>
2021-09-08 08:36:47 +0300

    Made trajectory visual more performant

--- Assets/Scripts/ShootTrajectory.cs
+++ Assets/Scripts/ShootTrajectory.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class ShootTrajectory : MonoBehaviour
 {
-    [SerializeField] int trajectoryDetail = 20;
+    [SerializeField] int trajectoryLenght = 20;
+    [SerializeField] int trajectoryDetail = 10;
     LineRenderer lr;
 
     private void Start()
@@ -15,8 +16,8 @@ public class ShootTrajectory : MonoBehaviour
 
     public void DrawTrajectory(Vector3 velocity)
     {
-        lr.positionCount = trajectoryDetail;
-        lr.SetPositions(Points(transform.position, velocity, trajectoryDetail));
+        lr.positionCount = trajectoryLenght;
+        lr.SetPositions(Points(transform.position, velocity, trajectoryLenght));
     }
 
     public void ResetTrajectory()
@@ -28,7 +29,7 @@ public class ShootTrajectory : MonoBehaviour
     {
         Vector3[] results = new Vector3[numberOfPoints];
 
-        float timeStep = Time.fixedDeltaTime / Physics.defaultSolverVelocityIterations;
+        float timeStep = trajectoryDetail * Time.fixedDeltaTime / Physics.defaultSolverVelocityIterations;
 
         Vector3 gravityAccel = Physics.gravity * timeStep * timeStep;
 
